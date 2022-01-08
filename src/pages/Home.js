afede26b4b2428c9d72be906a81009844cf70574
Diane Stephani <diane.stephani@gmail.com>
2022-01-07 20:40:43 -0800

    home page connected

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css'
+
+export default function Home() {
+    return (
+        <div className='Home'>
+            <p>This is the home page</p>
+
+        </div>
+    )
+}
